a5ef9feaa0bf5a2ade9f17d9238c5579d53cb529
adermiftahul <81670924+adermiftahul@users.noreply.github.com>
2021-04-22 10:41:51 +0700

    Update Tugas6_2.pas

--- Tugas 6/Tugas6_2.pas
+++ Tugas 6/Tugas6_2.pas
@@ -2,19 +2,23 @@ program Tugas6_2;
 Uses crt;
 
 Var
-nama, npm, alamat, jk, Nohp: String;
+nama, npm, alamat, ttl, jk, Nohp, email: String;
 
 Begin
 Write('Nama           : '); ReadLn(nama);
 Write('NPM            : '); ReadLn(npm);
 Write('Alamat         : '); ReadLn(alamat);
+Write('TTL            : '): Readln(ttl);
 Write('Jenis Kelamin  : '); ReadLn(jk);
 Write('No HP          : '); ReadLn(Nohp);
+Write('Email          : '); ReadLn(email);
 
 Writeln('DATA DIRI ANDA:');
 WriteLn('=============');
 WriteLn('Nama Anda Adalah "', nama, '" dengan NPM "', npm, '".');
-WriteLn('Anda Berjenis Kelamin "', jk, '" dan Beralamat di "', alamat, '".');
+WriteLn('Beralamat di "', alamat, '".');
+WriteLn('Tempat Tanggal Lahir "', ttl,'" dan Jenis Kelamin "', jk, '".');
 WriteLn('Nomor Kontak yang Bisa Dihubungi : ', Nohp, '.');
+WriteLn('Email Aktif : ', email, '.');
 readKey();
 End.
